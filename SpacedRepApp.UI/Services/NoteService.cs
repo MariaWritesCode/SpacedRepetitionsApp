bb41163db143f894a36797dd7a11a2fb13efdd93
Maria <MariaWritesCode@users.noreply.github.com>
2021-05-29 09:33:31 +0200

    revision works

--- SpacedRepApp.UI/Services/NoteService.cs
+++ SpacedRepApp.UI/Services/NoteService.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SpacedRepApp.UI.Services
